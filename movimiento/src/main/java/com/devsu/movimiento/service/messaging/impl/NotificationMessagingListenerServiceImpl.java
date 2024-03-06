a8c7c99b3d4c8236947a8009d1e57befd0bca0f0
Ruben Santopietro <rdsantopietro@gmail.com>
2024-03-06 16:14:31 -0300

    Added Gateway. Kafka was configured to no autoconfigure on startup when kafka propertie is not set in true.

--- movimiento/src/main/java/com/devsu/movimiento/service/messaging/impl/NotificationMessagingListenerServiceImpl.java
+++ movimiento/src/main/java/com/devsu/movimiento/service/messaging/impl/NotificationMessagingListenerServiceImpl.java
@@ -4,11 +4,13 @@ import com.devsu.movimiento.service.messaging.MessagingListenerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(name = "messaging.kafka", havingValue = "true", matchIfMissing = false)
 public class NotificationMessagingListenerServiceImpl implements MessagingListenerService {
 
     private final Logger log = LoggerFactory.getLogger(NotificationMessagingListenerServiceImpl.class);
