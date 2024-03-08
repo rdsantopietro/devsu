bfb2e3371133a11e33e57299e6ce87d229bbd718
Ruben Santopietro <rdsantopietro@gmail.com>
2024-03-08 18:46:40 -0300

    Added keycloak configuration

---
+++ gateway/src/main/java/com/devsu/gateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.devsu.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(withDefaults());
+        return http.build();
+    }
+}
